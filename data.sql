fb5cfbaebadeff90b8bbf9d9807fb664bf2d50b5
sboursen <dev.sboursen@gmail.com>
2022-04-27 14:07:51 +0100

    Add data into owners table

--- data.sql
+++ data.sql
@@ -108,3 +108,46 @@ INSERT INTO
     weight_kg
   )
 VALUES ('Ditto', 'May 14, 2022', 4, TRUE, 22);
+
+
+INSERT INTO
+  owners (
+    full_name,
+    age
+  )
+VALUES ('Sam Smith', 34);
+
+INSERT INTO
+  owners (
+    full_name,
+    age
+  )
+VALUES ('Jennifer Orwell', 19);
+
+INSERT INTO
+  owners (
+    full_name,
+    age
+  )
+VALUES ('Bob', 45);
+
+INSERT INTO
+  owners (
+    full_name,
+    age
+  )
+VALUES ('Melody Pond', 77);
+
+INSERT INTO
+  owners (
+    full_name,
+    age
+  )
+VALUES ('Dean Winchester', 14);
+
+INSERT INTO
+  owners (
+    full_name,
+    age
+  )
+VALUES ('Jodie Whittaker ', 38);
