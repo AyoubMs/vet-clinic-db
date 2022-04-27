4440eb5c24a6661e0deb7c89ef4eb64fb8eeaa6f
sboursen <dev.sboursen@gmail.com>
2022-04-27 14:20:49 +0100

    Update the species_id column in the animals table

--- data.sql
+++ data.sql
@@ -163,3 +163,12 @@ INSERT INTO
     name
   )
 VALUES ('Digimon');
+
+BEGIN;
+UPDATE animals
+SET species_id =
+CASE
+    WHEN name LIKE '%mon' THEN (SELECT id FROM species WHERE name = 'Digimon')
+    ELSE (SELECT id FROM species WHERE name = 'Pokemon')
+END;
+SELECT * FROM animals;
