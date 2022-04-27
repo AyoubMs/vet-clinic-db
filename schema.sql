4029eb9a6e51a59d3e327ffe3984afcc0eedf69a
sboursen <dev.sboursen@gmail.com>
2022-04-27 13:55:16 +0100

    Add owners_id foreign key the animals table

--- schema.sql
+++ schema.sql
@@ -41,3 +41,11 @@ ADD CONSTRAINT species_fk
 FOREIGN KEY (species_id)
 REFERENCES species(id)
 ON DELETE CASCADE;
+
+ALTER TABLE animals
+ADD COLUMN owner_id INT;
+ALTER TABLE animals
+ADD CONSTRAINT owner_fk
+FOREIGN KEY (owner_id)
+REFERENCES owners(id)
+ON DELETE CASCADE;
